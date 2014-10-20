b87ca48c24bab09a2fbe28d3bc70f42604be1052
xiangwei-wang <billwangxw@gmail.com>
2014-10-20 12:30:25 -0500

    Create new branch latest and remove the unused folders

--- Sample/LocalpointTester_iOS/Localpoint/LPLocalNotificationListener.m
+++ Sample/LocalpointTester_iOS/Localpoint/LPLocalNotificationListener.m
@@ -12,33 +12,33 @@
 @implementation LPLocalNotificationListener
 
 -(BOOL)onOverrideLocalNotification:(id<ILPLocalNotification>)notification {
-    if ([[notification getCampaignType] isEqualToString:@"GEOFENCE_ENTRY"]) {
-        // Custom the Geofence entry message
-        // Custom the notification title, it will display in an alert of notification tree
-        [notification setNotificationTitle:@"Entry custom: notification title"];
-        
-        // Custom the notification message, it will display in an alert of notification tree
-        [notification setNotificationMessage:@"Entry custom: notification message"];
-        
-        // Custom the campaign title, it will display in your offer list
-        [notification setCampaignTitle:@"Entry custom: campaign title"];
-        
-        // Custom the campaign body, this is the offer detail
-        [notification setCampaignBody:@"Entry custom: campaign body"];
-    } else if ([[notification getCampaignType] isEqualToString:@"GEOFENCE_EXIT"]) {
-        // Custom the Geofence entry message
-        // Custom the notification title, it will display in an alert of notification tree
-        [notification setNotificationTitle:@"Exit custom: notification title"];
-        
-        // Custom the notification message, it will display in an alert of notification tree
-        [notification setNotificationMessage:@"Exit custom: notification message"];
-        
-        // Custom the campaign title, it will display in your offer list
-        [notification setCampaignTitle:@"Exit custom: campaign title"];
-        
-        // Custom the campaign body, this is the offer detail
-        [notification setCampaignBody:@"Exit custom: campaign body"];
-    }
+//    if ([[notification getCampaignType] isEqualToString:@"GEOFENCE_ENTRY"]) {
+//        // Custom the Geofence entry message
+//        // Custom the notification title, it will display in an alert of notification tree
+//        [notification setNotificationTitle:@"Entry custom: notification title"];
+//        
+//        // Custom the notification message, it will display in an alert of notification tree
+//        [notification setNotificationMessage:@"Entry custom: notification message"];
+//        
+//        // Custom the campaign title, it will display in your offer list
+//        [notification setCampaignTitle:@"Entry custom: campaign title"];
+//        
+//        // Custom the campaign body, this is the offer detail
+//        [notification setCampaignBody:@"Entry custom: campaign body"];
+//    } else if ([[notification getCampaignType] isEqualToString:@"GEOFENCE_EXIT"]) {
+//        // Custom the Geofence entry message
+//        // Custom the notification title, it will display in an alert of notification tree
+//        [notification setNotificationTitle:@"Exit custom: notification title"];
+//        
+//        // Custom the notification message, it will display in an alert of notification tree
+//        [notification setNotificationMessage:@"Exit custom: notification message"];
+//        
+//        // Custom the campaign title, it will display in your offer list
+//        [notification setCampaignTitle:@"Exit custom: campaign title"];
+//        
+//        // Custom the campaign body, this is the offer detail
+//        [notification setCampaignBody:@"Exit custom: campaign body"];
+//    }
     
     // If return 'NO', SDK will send local notification base on this ILPLocalNotification object
     // If return 'YES', SDK will not send local notification, but will keep this ILPLocalNotification, you can get it through the APIs in message provider.
